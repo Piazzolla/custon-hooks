6b899815314d730f257d59cf45076e6b381d2a2e
Mariano <mariano.burgos@pjn.gov.ar>
2022-09-06 15:15:40 -0300

    primer commit

---
+++ useTodo.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useReducer } from 'react'
+import { todoReducer } from '../08-useReducer/todoReducer'
+
+
+const initialState = [{
+    id: 1,
+    descrption: 'Piedra del Alma',
+    done: false
+}]
+
+const init = () => {
+    let res = JSON.parse(localStorage.getItem('todos'));
+   
+    if (res)
+        return res;
+    else
+        return [];
+    //JSON.parse(localStorage.getItem('todos') || [])
+    //return initialState;
+}
+
+
+export const useTodo = () => {
+
+    const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init)
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }, [todos]);
+
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+        dispatchTodo(action);
+    }
+
+    const handleDeleteTodo = (id) => {
+        dispatchTodo({
+            type: '[TODO] Remove Todo',
+            payload: id
+        })
+    }
+
+
+    const handleToggleTodo = (id) => {
+        dispatchTodo({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        })
+    }
+
+
+    return {
+        todos,
+        handleDeleteTodo,
+        handleNewTodo,
+        handleToggleTodo,
+        todoCount: todos.length,
+        pendingTodosCount: todos.filter(todo => !todo.done).length
+    }
+}
+
